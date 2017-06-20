486ae468c1f0be6c03b3cccd46bfff1799397892
Cody A. Coleman <codyaustun@gmail.com>
2017-06-20 10:58:44 -0700

    Initial commit with Resnet code for CIFAR10

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='resnet',
+    version='0.0.0',
+    url='http://www.codycoleman.com',
+    author='Cody Austun Coleman',
+    author_email='cody.coleman@cs.stanford.edu',
+    packages=['resnet'],
+    install_requires=[
+        'torchvision',
+    ]
+)
